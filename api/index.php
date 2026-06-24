5b1efef6e2ae63cb2e80208faab8aeb03958f461
Dannn17 <hamdannaim@student.ub.ac.id>
2026-06-24 21:28:11 +0700

    Debug: Force enable APP_DEBUG for tracking error 500

--- api/index.php
+++ api/index.php
@@ -1,12 +1,24 @@
 <?php
 
-// Mengarahkan ke file autoload bawaan vendor Laravel
+// Paksa Laravel memunculkan detail error ke browser
+$_ENV['APP_ENV'] = 'production';
+$_ENV['APP_DEBUG'] = 'true';
+$_ENV['CACHE_DRIVER'] = 'array';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+
+// Buat database SQLite otomatis jika belum ada
+$dbPath = '/tmp/database.sqlite';
+if (!file_exists($dbPath)) {
+    touch($dbPath);
+}
+$_ENV['DB_CONNECTION'] = 'sqlite';
+$_ENV['DB_DATABASE'] = $dbPath;
+
+// Jalankan bootstrap Laravel
 require __DIR__ . '/../vendor/autoload.php';
 
-// Menjalankan aplikasi bootstrap Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Menangani request yang masuk ke server
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
